1d5d6e3ef58f90a1042c0371b8610cc285e08eb9
Matthias Schild <matthias@matthiasschild.de>
2021-02-28 02:10:15 +0100

    Added database connection.

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,24 @@
+const collections = require('./database.js')
 const express = require('express');
 const cors = require('cors')
 const app = express()
+
 const port = 3001
 
 app.use(cors())
 
 app.get('/fetch', (req, res) => {
-    res.send([
-        {
-            name: 'temperature',
-            value: 512,
-        },
-        {
-            name: 'angle',
-            value: 128
+    collections.machines.find().toArray((err, result) => {
+        if (err) throw err;
+
+        for (let r of result) {
+            if (r.demo) {
+                r.value += (Math.random() - 0.5)
+            }
         }
-    ])
+
+        res.send(result)
+    })
 })
 
 app.listen(port, () => {
