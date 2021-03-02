d191590d7c3168ac1d88d667afe12f6d5e6b5589
Matthias Schild <matthias@matthiasschild.de>
2021-03-02 13:17:23 +0100

    Added gimmicky data visualization.

--- Frontend/src/components/ValueItem.tsx
+++ Frontend/src/components/ValueItem.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import {IonItem, IonLabel, IonToggle} from '@ionic/react'
 import {MachineValuePair} from '../api/models'
 import {sprintf} from 'sprintf-js'
+import {
+    FlexibleXYPlot,
+    HorizontalGridLines,
+    LineMarkSeries,
+    LineSeries,
+    VerticalGridLines,
+    XAxis,
+    XYPlot,
+    YAxis,
+} from 'react-vis'
 
 interface Props {
     valuePair: MachineValuePair
@@ -10,15 +20,35 @@ interface Props {
 
 
 interface State {
+    dataCache: {[index: string]: number[]}
 }
 
 export default class ValueItem extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            dataCache: {}
+        }
+    }
+
     private formatNumber(valuePair: MachineValuePair): string {
         if (valuePair.format) {
             return sprintf(valuePair.format, valuePair.value)
         }
 
         return valuePair.value.toFixed(2)
+    }
+
+    private dataForPair(valuePair: MachineValuePair) {
+        const key = `${valuePair.name}` // TODO differentiate between machines
+        let line = this.state.dataCache[key] || []
+
+        line.push(valuePair.value)
+        line = line.splice(-30) // limit the size
+        this.state.dataCache[key] = line
+
+        return line.map((val, index) => ({x: index, y: val}))
     }
 
     render() {
@@ -42,6 +72,9 @@ export default class ValueItem extends React.Component<Props, State> {
                         <IonLabel>
                             <h1>{valuePair.name}</h1>
                             <h2>{this.formatNumber(valuePair)}</h2>
+                            <FlexibleXYPlot height={100}>
+                                <LineSeries data={this.dataForPair(valuePair)} style={{ fill: 'none' }}/>
+                            </FlexibleXYPlot>
                         </IonLabel>
                     </IonItem>
                 )
